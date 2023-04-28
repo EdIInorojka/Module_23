63c3bb2bc08b1fd0ee44cf3a7606e389a77d33aa
Koh Aleksey <alexckokh@gmail.com>
2023-04-28 11:44:43 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp23/Converter.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Converter
+    {
+        /// <summary>
+        /// метод для перевода строкового представления числа в тип Double
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static double StringToDouble(string str)
+        {
+            bool isNegative;
+
+            var (intPart, fractPart) = StringFiltering(str, out isNegative);
+
+            int integer = StringToInt(intPart); //целая часть 
+            double fraction = IntToFract(StringToInt(fractPart)); //дробная часть
+
+            double number = integer + fraction;
+
+            if (isNegative)
+                number *= -1;
+
+            return number;
+        }
+        /// <summary>
+        /// метод разделения строкового представления числа на целую и дробную части
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="isNegative"></param>
+        /// <returns></returns>
+        public static (string, string) StringFiltering(string input, out bool isNegative)
+        {
+            List<char> charList = new List<char>();
+
+            bool commaFlag = false;
+            bool firstZeroFlag = false;  
+            bool firstZeroNegativeFlag = false;
+            isNegative = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!isNegative && input[0] == '-') // определение знака числа
+                {
+                    isNegative = true;
+                }
+
+                if (!firstZeroFlag && (input[0] == '.' || input[0] == ',')) //добавление в лист целой части если она равна 0
+                {
+                    charList.Add('0');
+                    firstZeroFlag = true;
+                }
+
+                if (isNegative && !firstZeroNegativeFlag && (input[1] == '.' || input[1] == ',')) //добавление в лист всей целой части неравной 0
+                {
+                    charList.Add('0');
+                    firstZeroNegativeFlag = true;
+                }
+
+                if (!commaFlag && (input[i] == '.' || input[i] == ',')) //добавлеие в лист знака разделителя
+                {
+                    charList.Add(input[i]);
+                    commaFlag = true;
+                    continue;
+                }
+
+                if (char.IsDigit(input[i])) // закидывание в лист всех оставшихся чисел
+                {
+                    charList.Add(input[i]);
+                }
+            }
+
+            string numberInString = string.Join("", charList); // соединение элементов листа в целое число в строковом виде
+
+            string intPart = numberInString.Split(new char[] { ',', '.' })[0]; //выделение целой части
+            string fractPart = numberInString.Split(new char[] { ',', '.' })[1]; //выделение дробной части
+
+            return (intPart, fractPart);
+        }
+        /// <summary>
+        /// метод перевода строкового числа (целая часть) в числовое представление
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int StringToInt(string str)
+        {
+            if (str.Length == 1)
+                return (str[0] - '0');
+
+            int next = StringToInt(str.Substring(1));
+            int res = str[0] - '0';
+
+            res = (int)(res * Math.Pow(10, str.Length - 1) + next);
+
+            return res;
+        }
+        /// <summary>
+        /// метод перевода строкового числа (дробной часть) в числовое представление
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static double IntToFract(int number)
+        {
+            int length = (int)Math.Floor(Math.Log10(number) + 1);
+            long factor = (long)Math.Pow(10, length);
+            double result = (double)number / factor;
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            double num = Converter.StringToDouble(str);
+
+            Console.WriteLine(num);
+        }
+    }
+}
